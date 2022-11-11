7fdc049ca27d62ce2ff47a4f9b19912bf71f788d
Abdul Rehman <beingabdulr@gmail.com>
2022-11-11 04:52:24 -0800

    Update Header

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const headerToggle = document.querySelector('.header-toggle')
 const primaryNav = document.getElementById('primary-navigation')
 
 headerToggle.addEventListener('click', () => {
-  const ariaExp = !headerToggle.getAttribute
-  console.log('click')
+  var ariaExp = headerToggle.getAttribute('aria-expanded') === 'false' ? true : false
+
   headerToggle.setAttribute('aria-expanded', ariaExp)
   primaryNav.setAttribute('data-opened', ariaExp)
 })
